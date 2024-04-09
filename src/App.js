19988c55d962812924e1e716ee71ffff47a78195
Steven Luo <stvncool6@gmail.com>
2024-04-09 02:49:23 -0600

    Add localStorage feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Form from "./components/Form";
 import Stats from "./components/Stats";
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(
+    () => JSON.parse(localStorage.getItem("items")) || []
+  );
   const handleAddItems = (i) => setItems([...items, i]);
   const handleDeleteItem = (id) =>
     setItems((items) => items.filter((item) => item.id !== id));
@@ -25,6 +27,10 @@ export default function App() {
       if (confirmed) setItems([]);
     }
   };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   return (
     <div className="app">
